1d570dac788a05634d8af079d97d07c2eca01c5f
pannatorn <57486797+pannatron@users.noreply.github.com>
2024-04-10 09:36:13 +0700

    Create excersise_1_2.py

---
+++ excersise_1_2.py
@@ -0,0 +1,7 @@
+# ฟังก์ชันสำหรับบวกเลขจาก 2 ตัวแปร input
+def sum_two_numbers(num1, num2):
+    return f"sum total = {num1 + num2}"
+
+# ตัวอย่างการใช้งานฟังก์ชัน
+print(sum_two_numbers(8, 9))
+print(sum_two_numbers(13, -2))
