0040e082766a975d7df52a7bdbeebbaad624fbe5
pannatorn <57486797+pannatron@users.noreply.github.com>
2024-04-10 09:54:12 +0700

    Create excersise_2_3.py

---
+++ excersise_2_3.py
@@ -0,0 +1,11 @@
+# ฟังก์ชันสำหรับแปลงอุณหภูมิ
+def convert_temperature(mode, temperature):
+    # เช็ค mode และคำนวณอุณหภูมิตามสูตร
+    if mode == "C -> F":
+       "เติมโปรแกรมทีนี่"
+    elif mode == "F -> C":
+       "เติมโปรแกรมทีนี่"
+
+# ตัวอย่างการใช้งานฟังก์ชัน
+print(convert_temperature("C -> F", 100)) # ควรแสดงผลลัพธ์: 212.0 Fahrenheit
+print(convert_temperature("F -> C", 32)) # ควรแสดงผลลัพธ์: 0.0 Celsius
