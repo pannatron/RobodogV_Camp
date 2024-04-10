4fc53bf00d9de526117f1d5e3f3467eeccc01d7d
pannatorn <57486797+pannatron@users.noreply.github.com>
2024-04-10 09:34:05 +0700

    Create excersise_1_1.py

---
+++ excersise_1_1.py
@@ -0,0 +1,7 @@
+# ฟังก์ชันสำหรับรับค่าอายุแล้วพิมพ์ประโยค
+def print_age_sentence(age):
+    return f"I'm {age} years old"
+
+# ตัวอย่างการใช้งานฟังก์ชัน
+print(print_age_sentence(18))
+print(print_age_sentence(19))
