a6f97614f64477586c9b78aedc2797d63153cb15
pannatorn <57486797+pannatron@users.noreply.github.com>
2024-04-10 12:24:53 +0700

    Create excersise_4_1.py

---
+++ excersise_4_1.py
@@ -0,0 +1,10 @@
+# กำหนดลิสต์ของตัวเลข
+List1 = [9, 10, 16, 2, 18]
+
+# หาค่าสูงสุดและต่ำสุด
+max_value = max(List1)
+min_value = min(List1)
+
+# แสดงผลลัพธ์
+print(f"max = {max_value}")
+print(f"min = {min_value}")
