85c8a9bfd33192587b83ffb8accbc62197041361
pannatorn <57486797+pannatron@users.noreply.github.com>
2024-04-10 13:05:24 +0700

    Create excersise_4_5_advance.py

---
+++ excersise_4_5_advance.py
@@ -0,0 +1,8 @@
+# กำหนดลิสต์ของตัวเลข
+List1 = [1, 2, 3, 4, 5, 6, 7, 8, 9, 0]
+
+# แปลงลิสต์เป็นสตริงรูปแบบเบอร์โทรศัพท์
+phone_number_format = f"({List1[0]}{List1[1]}{List1[2]}) {List1[3]}{List1[4]}{List1[5]}-{List1[6]}{List1[7]}{List1[8]}{List1[9]}"
+
+# แสดงผลลัพธ์
+print(phone_number_format)
