d6101d3a3101691457895f4836e71d38e3f5350f
pannatorn <57486797+pannatron@users.noreply.github.com>
2024-04-12 10:14:39 +0700

    Create exercise_6_3.py

---
+++ exercise_6_3.py
@@ -0,0 +1,12 @@
+import cv2
+
+# Read an image
+image = cv2.imread('image/Cat03.jpg')
+
+# Convert the image to grayscale
+gray_image = cv2.cvtColor(image, cv2.COLOR_BGR2GRAY)
+
+# Display the grayscale image
+cv2.imshow('Grayscale Image', gray_image)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
